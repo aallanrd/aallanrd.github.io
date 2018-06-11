197adb3641fdd8e0aad04ca9bba53679be2eca1f
Allan Rojas <allan.rojas@microvention.com>
2018-06-10 21:36:50 -0600

    Change

--- js/todo.js
+++ js/todo.js
@@ -1,42 +1,43 @@
 angular.module('App', [])
 .controller('Controller', ['$scope', function($scope) {
-  
-  
+
+
 
   $scope.generate = function() {
-    
-	$scope.calendars = [];
-	
-	
-	var nb=$('#date').val();
-	
-	var date = new Date(nb);
-	
-	var start = 0;
-	
+
+	   $scope.calendars = [];
+
+
+	  var nb=$('#date').val();
+
+	  var date = new Date(nb);
+    alert(date);
+	  var start = 0;
+  
+
     while(start < $scope.number){
-		
+
 		//Pushing Calendar to Array
-	
-		newCalendar = {
-			
-			"id": start,
-			"date": date
-		 };
-			 
-		$scope.calendars.push(newCalendar);
-			
-		
+
+  		newDays = {
+
+  			"id": start,
+  			"date": date
+  		 };
+
+		$scope.calendars.push(newDays);
+
+
 		//Adding days to calendar
-		date.setDate(date.getDate() + 7);
-		
+		date.setDate(date.getDate() + 1);
+
 		//Add week days to count variable
-		start = start + 7;
-		
-	}	
-	
-	
+		start = start + 1;
+
+	}
+
+
 	console.log("Ended..");
-	
+
   };
 }]);
